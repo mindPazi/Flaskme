8cdb092fcf3627ad377708e7bba674ab953f3344
mindPazi <gitmind87@gmail.com>
2025-03-22 21:59:17 +0100

    funct

---
+++ src/main/kotlin/org/jetbrains/plugins/template/listeners/MyApplicationActivationListener.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.template.listeners
+
+import com.intellij.openapi.application.ApplicationActivationListener
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.wm.IdeFrame
+
+internal class MyApplicationActivationListener : ApplicationActivationListener {
+
+    override fun applicationActivated(ideFrame: IdeFrame) {
+        thisLogger().warn("Placeholder warning message for applicationActivated")
+    }
+}
---
+++ src/main/kotlin/org/jetbrains/plugins/template/services/MyProjectService.kt
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.template.services
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.diagnostic.Logger
+
+@Service(Service.Level.PROJECT)
+class MyProjectService(private val project: Project) {
+    private val LOG = Logger.getInstance(MyProjectService::class.java)
+
+    init {
+        LOG.info("🔧 MyProjectService initialized for ${project.name}")
+    }
+
+    fun getRandomNumber(): Int = (0..100).random()
+}
