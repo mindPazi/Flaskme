dbd480596ff9367477c60fe40777cacdfa05f33a
mindPazi <gitmind87@gmail.com>
2025-03-24 16:43:00 +0100

    Comm

--- src/main/kotlin/org/jetbrains/plugins/template/services/MyProjectService.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/services/MyProjectService.kt
@@ -4,20 +4,18 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.thisLogger
+import com.jetbrains.python.psi.PyElementGenerator
+import com.jetbrains.python.psi.LanguageLevel
+
+
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(private val project: Project) {
 
     init {
-        thisLogger().info("Placeholder warning message for MyProjectService")
-        thisLogger().info("Placeholder warning message for MyProjectService")
-        thisLogger().info("Placeholder warning message for MyProjectService")
-        thisLogger().info("Placeholder warning message for MyProjectService")
-        thisLogger().info("Placeholder warning message for MyProjectService")
-        thisLogger().info("Placeholder warning message for MyProjectService")
-        thisLogger().info("Placeholder warning message for MyProjectService")
-        thisLogger().info("Placeholder warning message for MyProjectService")
-
+        val generator = PyElementGenerator.getInstance(project)
+        val pyExpr = generator.createExpressionFromText(LanguageLevel.getDefault(), "42 + 1")
+        thisLogger().info("Generated Python expression: ${pyExpr.text}")
     }
 
     fun getRandomNumber(): Int = (0..100).random()
